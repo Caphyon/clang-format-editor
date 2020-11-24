c08609ca1933a892fde62dd1342d9ea89c6ee11e
horatiu.prica <horatiu.prica@caphyon.com>
2020-11-24 15:42:08 +0200

    Helper classes

---
+++ ClangFormatDetector/Helpers/FormatOptionFile.cs
@@ -0,0 +1,173 @@
+﻿using ClangFormatDetector.Enums;
+using ClangFormatDetector.Interfaces;
+using ClangFormatDetector.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClangFormatDetector.Helpers
+{
+  public class FormatOptionFile
+  {
+    public static StringBuilder CreateOutput(List<IFormatOption> formatOptions, FormatStyle style)
+    {
+      List<IFormatOption> options;
+      var output = new StringBuilder();
+      output.AppendLine("# Format Style Options - Created with Clang Power Tools");
+      output.AppendLine("---");
+      output.AppendLine("Language: Cpp");
+
+      switch (style)
+      {
+        case FormatStyle.LLVM:
+          output.AppendLine("BasedOnStyle: LLVM");
+          options = GetChangedOptions(formatOptions, new FormatOptionsLlvmData().FormatOptions);
+          AddActiveOptionToFile(options, output);
+          break;
+        case FormatStyle.Google:
+          output.AppendLine("BasedOnStyle: Google");
+          options = GetChangedOptions(formatOptions, new FormatOptionsGoogleData().FormatOptions);
+          AddActiveOptionToFile(options, output);
+          break;
+        case FormatStyle.Chromium:
+          output.AppendLine("BasedOnStyle: Chromium");
+          options = GetChangedOptions(formatOptions, new FormatOptionsChromiumData().FormatOptions);
+          AddActiveOptionToFile(options, output);
+          break;
+        case FormatStyle.Mozilla:
+          output.AppendLine("BasedOnStyle: Mozilla");
+          options = GetChangedOptions(formatOptions, new FormatOptionsMozillaData().FormatOptions);
+          AddActiveOptionToFile(options, output);
+          break;
+        case FormatStyle.WebKit:
+          output.AppendLine("BasedOnStyle: WebKit");
+          options = GetChangedOptions(formatOptions, new FormatOptionsWebKitData().FormatOptions);
+          AddActiveOptionToFile(options, output);
+          break;
+        case FormatStyle.Microsoft:
+          output.AppendLine("BasedOnStyle: Microsoft");
+          options = GetChangedOptions(formatOptions, new FormatOptionsMicrosoftData().FormatOptions);
+          AddActiveOptionToFile(options, output);
+          break;
+        default:
+          AddActiveOptionToFile(formatOptions, output);
+          break;
+      }
+      output.AppendLine("...");
+
+      return output;
+    }
+
+    private static List<IFormatOption> GetChangedOptions(List<IFormatOption> currentOptions, List<IFormatOption> defaultOptions)
+    {
+      var optionsToInclude = new List<IFormatOption>();
+      for (int i = 0; i < currentOptions.Count; i++)
+      {
+        if (currentOptions[i] is FormatOptionToggleModel)
+        {
+          var currentOption = currentOptions[i] as FormatOptionToggleModel;
+          var defaultOption = defaultOptions[i] as FormatOptionToggleModel;
+          if (currentOption.BooleanCombobox == defaultOption.BooleanCombobox)
+          {
+            continue;
+          }
+        }
+        else if (currentOptions[i] is FormatOptionInputModel)
+        {
+          var currentOption = currentOptions[i] as FormatOptionInputModel;
+          var defaultOption = defaultOptions[i] as FormatOptionInputModel;
+          if (string.Compare(currentOption.Input, defaultOption.Input) == 0 || string.IsNullOrEmpty(currentOption.Input))
+          {
+            continue;
+          }
+        }
+        else if (currentOptions[i] is FormatOptionMultipleInputModel)
+        {
+          var currentOption = currentOptions[i] as FormatOptionMultipleInputModel;
+          var defaultOption = defaultOptions[i] as FormatOptionMultipleInputModel;
+          if (string.Compare(currentOption.MultipleInput, defaultOption.MultipleInput) == 0 || string.IsNullOrEmpty(currentOption.MultipleInput))
+          {
+            continue;
+          }
+        }
+        else if (currentOptions[i] is FormatOptionMultipleToggleModel)
+        {
+          var currentOption = currentOptions[i] as FormatOptionMultipleToggleModel;
+          var defaultOption = defaultOptions[i] as FormatOptionMultipleToggleModel;
+          var toggleFlags = RemoveUnchagedToogleFlags(currentOption.ToggleFlags, defaultOption.ToggleFlags);
+
+          if (toggleFlags.Count == 0)
+          {
+            continue;
+          }
+          else
+          {
+            var formatOptionMultipleToggleModel = new FormatOptionMultipleToggleModel
+            {
+              ToggleFlags = toggleFlags,
+              Name = currentOption.Name
+            };
+
+            optionsToInclude.Add(formatOptionMultipleToggleModel);
+            continue;
+          }
+        }
+        optionsToInclude.Add(currentOptions[i]);
+      }
+
+      return optionsToInclude;
+    }
+
+    private static void AddActiveOptionToFile(List<IFormatOption> formatOptions, StringBuilder output)
+    {
+      foreach (var item in formatOptions)
+      {
+        if (item.IsEnabled == false) continue;
+
+        var styleOption = string.Empty;
+        switch (item)
+        {
+          case FormatOptionToggleModel option:
+            styleOption = string.Concat(option.Name, ": ", option.BooleanCombobox.ToString().ToLower());
+            break;
+          case FormatOptionInputModel option when string.IsNullOrEmpty(option.Input) == false:
+            styleOption = string.Concat(option.Name, ": ", option.Input);
+            break;
+          case FormatOptionMultipleInputModel option when string.IsNullOrEmpty(option.MultipleInput) == false:
+            styleOption = string.Concat(option.Name, ": \r\n", option.MultipleInput);
+            break;
+          case FormatOptionMultipleToggleModel option:
+            styleOption = string.Concat(option.Name, ": \r\n", CreateMultipleToggleFlag(option.ToggleFlags));
+            break;
+          default:
+            break;
+        }
+
+        output.AppendLine(styleOption);
+      }
+    }
+
+    private static List<ToggleModel> RemoveUnchagedToogleFlags(List<ToggleModel> currentOption, List<ToggleModel> defaultOption)
+    {
+      var modifedFlags = new List<ToggleModel>();
+      for (int index = 0; index < currentOption.Count; index++)
+      {
+        if (currentOption[index].Value != defaultOption[index].Value)
+        {
+          modifedFlags.Add(currentOption[index]);
+        }
+      }
+      return modifedFlags;
+    }
+
+    private static string CreateMultipleToggleFlag(List<ToggleModel> toggleModels)
+    {
+      var sb = new StringBuilder();
+      foreach (var item in toggleModels)
+      {
+        sb.AppendLine(string.Concat("  ", item.Name, ": ", item.Value.ToString().ToLower()));
+      }
+      return sb.ToString().TrimEnd();
+    }
+  }
+}
+
---
+++ ClangFormatDetector/Helpers/FormatOptionsProvider.cs
@@ -0,0 +1,97 @@
+﻿using ClangFormatDetector.Enums;
+using ClangFormatDetector.Extensions;
+using ClangFormatDetector.Interfaces;
+using ClangFormatDetector.Models;
+using System.Collections.Generic;
+
+namespace ClangFormatDetector
+{
+  public class FormatOptionsProvider
+  {
+    #region Members
+
+    public static FormatOptionsLlvmData LlvmOptionsData;
+    public static FormatOptionsGoogleData GoogleOptionsData;
+    public static FormatOptionsChromiumData ChromiumOptionsData;
+    public static FormatOptionsMozillaData MozillaOptionsData;
+    public static FormatOptionsWebKitData WebkitOptionsData;
+    public static FormatOptionsMicrosoftData MicrosoftOptionsData;
+
+    #endregion
+
+    #region Constructor
+
+    static FormatOptionsProvider()
+    {
+      InitializeFormatData();
+    }
+
+    #endregion
+
+    #region Methods
+    public static void ResetOptions()
+    {
+      InitializeFormatData();
+    }
+
+    public static List<IFormatOption> GetDefaultOptionsForStyle(FormatStyle style)
+    {
+      switch (style)
+      {
+        case FormatStyle.LLVM:
+          return new FormatOptionsLlvmData().FormatOptions;
+        case FormatStyle.Google:
+          return new FormatOptionsGoogleData().FormatOptions;
+        case FormatStyle.Chromium:
+          return new FormatOptionsChromiumData().FormatOptions;
+        case FormatStyle.Microsoft:
+          return new FormatOptionsMicrosoftData().FormatOptions;
+        case FormatStyle.Mozilla:
+          return new FormatOptionsMozillaData().FormatOptions;
+        case FormatStyle.WebKit:
+          return new FormatOptionsWebKitData().FormatOptions;
+        default:
+          break;
+      }
+      return new FormatOptionsLlvmData().FormatOptions;
+    }
+
+    public static List<IFormatOption> CloneDetectedOptions(List<IFormatOption> formatOptions)
+    {
+      var clonedOptions = new List<IFormatOption>();
+      foreach (var option in formatOptions)
+      {
+        switch (option)
+        {
+          case FormatOptionToggleModel toggleModel:
+            clonedOptions.Add(toggleModel.Clone());
+            break;
+          case FormatOptionInputModel inputModel:
+            clonedOptions.Add(inputModel.Clone());
+            break;
+          case FormatOptionMultipleToggleModel multipleToggleModel:
+            clonedOptions.Add(multipleToggleModel.Clone());
+            break;
+          case FormatOptionMultipleInputModel multipleInputModel:
+            clonedOptions.Add(multipleInputModel.Clone());
+            break;
+          default:
+            break;
+        }
+      }
+      return clonedOptions;
+    }
+
+    private static void InitializeFormatData()
+    {
+      LlvmOptionsData = new FormatOptionsLlvmData();
+      GoogleOptionsData = new FormatOptionsGoogleData();
+      ChromiumOptionsData = new FormatOptionsChromiumData();
+      MozillaOptionsData = new FormatOptionsMozillaData();
+      WebkitOptionsData = new FormatOptionsWebKitData();
+      MicrosoftOptionsData = new FormatOptionsMicrosoftData();
+    }
+
+    #endregion
+  }
+}
