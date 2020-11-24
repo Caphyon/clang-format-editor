fa9afd222dbf07d07c17df9ad420aa44854d7b7a
horatiu.prica <horatiu.prica@caphyon.com>
2020-11-24 15:40:38 +0200

    Add extension methods

---
+++ ClangFormatDetector/Extensions/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace ClangFormatDetector.Extensions
+{
+  public static class StringExtensions
+  {
+    public static string SubstringBefore(this string aText, string aSearchedSubstring)
+    {
+      if (string.IsNullOrEmpty(aSearchedSubstring))
+        return aSearchedSubstring;
+
+      CompareInfo compareInfo = CultureInfo.InvariantCulture.CompareInfo;
+      int index = compareInfo.IndexOf(aText, aSearchedSubstring, CompareOptions.Ordinal);
+
+      if (index < 0)
+        return string.Empty; //No such substring
+
+      return aText.Substring(0, index);
+    }
+
+    public static string SubstringAfter(this string aText, string aSearchedSubstring)
+    {
+      if (string.IsNullOrEmpty(aSearchedSubstring))
+        return aText;
+
+      CompareInfo compareInfo = CultureInfo.InvariantCulture.CompareInfo;
+      int index = compareInfo.IndexOf(aText, aSearchedSubstring, CompareOptions.Ordinal);
+
+      if (index < 0)
+        return string.Empty; //No such substring
+
+      return aText.Substring(index + aSearchedSubstring.Length);
+    }
+  }
+}
