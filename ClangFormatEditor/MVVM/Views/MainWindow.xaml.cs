b29c2a844a26be151e2f823c32821f9850c6a6df
horatiu.prica <horatiu.prica@caphyon.com>
2021-01-22 17:09:52 +0200

    Fix issues after import (missing images, broken references) and change entry point to MainWindow

---
+++ ClangFormatEditor/MVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ClangFormatEditor.MVVM.Views
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var selectorView = new FileSelectorView();
+      selectorView.Show();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      var editorView = new FormatEditorView();
+      editorView.Show();
+    }
+  }
+}
