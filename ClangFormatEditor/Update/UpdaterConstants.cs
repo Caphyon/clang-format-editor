fc40b6e77fa77b9cfe122ab536c2376afb908257
horatiu.prica <horatiu.prica@caphyon.com>
2021-02-10 18:04:09 +0200

    Updater constants

---
+++ ClangFormatEditor/Update/UpdaterConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace ClangFormatEditor.Update
+{
+  public static class UpdaterConstants
+  {
+    public const string Path = @"C:\Program Files (x86)\Caphyon\Clang Format Editor";
+    public const string Executable = "Clang Format Editor Updater.exe";
+    public const string CheckUpdate = "/checknow - minuseractions";
+    public const string Cmd = "cmd.exe";
+    public const string CommandParamater = "/C ";
+    public const int NoUpdateReturnCode = -536870895;
+    public const int UpdateFoundReturnCode = 0;
+  }
+}
