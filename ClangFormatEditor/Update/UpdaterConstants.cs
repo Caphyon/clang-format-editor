e1e3002b3321a80386bec7f02e1c95a36f20a3fa
horatiu.prica <horatiu.prica@caphyon.com>
2021-02-11 13:39:51 +0200

    Updater constant for Visual Studio parameter

--- ClangFormatEditor/Update/UpdaterConstants.cs
+++ ClangFormatEditor/Update/UpdaterConstants.cs
@@ -6,6 +6,7 @@
     public const string Executable = "Clang Format Editor Updater.exe";
     public const string CheckUpdate = "/checknow - minuseractions";
     public const string Cmd = "cmd.exe";
+    public const string VisualStudio = "VisualStudio";
     public const string CommandParamater = "/C ";
     public const int NoUpdateReturnCode = -536870895;
     public const int UpdateFoundReturnCode = 0;
