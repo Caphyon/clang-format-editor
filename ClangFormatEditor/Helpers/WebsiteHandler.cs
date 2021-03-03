27c740fb9f948bbdf4311924d542cbe2d47d6cd2
horatiu.prica <horatiu.prica@caphyon.com>
2021-03-03 18:14:13 +0200

    WebsiteHandler

---
+++ ClangFormatEditor/Helpers/WebsiteHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ClangFormatEditor.Helpers
+{
+  public class WebsiteHandler
+  {
+    public static void OpenUri(string uri)
+    {
+      try
+      {
+        var processStartInfo = new ProcessStartInfo(uri)
+        {
+          UseShellExecute = true,
+        };
+        Process.Start(processStartInfo);
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message, "Clang-Format Editor Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+    }
+  }
+}
