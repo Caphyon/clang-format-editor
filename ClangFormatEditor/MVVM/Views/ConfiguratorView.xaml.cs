0639322712cb6e110231436e3e9caea5b8b1e723
horatiu.prica <horatiu.prica@caphyon.com>
2021-03-17 20:53:07 +0200

    Move constant and remove RunFormat_Editor event

--- ClangFormatEditor/MVVM/Views/ConfiguratorView.xaml.cs
+++ ClangFormatEditor/MVVM/Views/ConfiguratorView.xaml.cs
@@ -11,11 +11,6 @@ namespace ClangFormatEditor.MVVM.Views
   {
     private readonly ConfiguratorViewModel configuratorViewModel;
 
-    private const string inputWindowDefaulText = "// --- Clang Power Tools - Format Style Editor ---\r\n//\r\n" +
-                                                 "// Add your code here\r\n//\r\n" +
-                                                 "// Turn ON any format option or select a Style\r\n//\r\n" +
-                                                 "// Format is run automatically\r\n//\r\n" +
-                                                "// Check the OUTPUT to see your formatted code";
     private const string outputWindowDefaulText = "// Your formatted code is displayed here";
 
     public ConfiguratorView()
@@ -23,18 +18,11 @@ namespace ClangFormatEditor.MVVM.Views
       InitializeComponent();
       configuratorViewModel = new ConfiguratorViewModel(this);
       DataContext = configuratorViewModel;
-      CodeEditor.Text = inputWindowDefaulText;
       CodeEditorReadOnly.Text = outputWindowDefaulText;
     }
 
     private void RunFormat_TextBoxChanged(object sender, TextChangedEventArgs e)
     {
-      configuratorViewModel.RunFormat();
-    }
-
-    private void RunFormat_Editor(object sender, EventArgs e)
-    {
-      if (configuratorViewModel.IsAnyOptionEnabled() == false) return;
       configuratorViewModel.RunFormat();
     }
 
